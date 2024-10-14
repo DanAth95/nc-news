43b7858babbaabbe7b42a0ebf2eaeb0a5b9deaa8
DanAth95 <daniel_atherton@aol.co.uk>
2024-10-14 11:19:57 +0100

    add get api/topics and relevant testing for 200 and invalid api 404

---
+++ models/topics.model.js
@@ -0,0 +1,10 @@
+const db = require("../db/connection");
+
+exports.fetchTopics = () => {
+  return db.query("SELECT * FROM topics").then(({ rows }) => {
+    if (rows.length === 0) {
+      return Promise.reject({ status: 404, msg: "Topics Not Found" });
+    }
+    return rows;
+  });
+};
