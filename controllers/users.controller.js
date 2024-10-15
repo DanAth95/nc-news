f40e4fa09b13619c181bc0522f3f1303fdb7af2d
DanAth95 <daniel_atherton@aol.co.uk>
2024-10-15 14:03:49 +0100

    add get user by username and relevant testing

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,9 +1,20 @@
-const { fetchUsers } = require("../models/users.model");
+const { fetchUsers, fetchUserByUsername } = require("../models/users.model");
 
 exports.getUsers = (req, res, next) => {
   return fetchUsers()
     .then((users) => {
       res.send({ users });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.getUserByUsername = (req, res, next) => {
+  const username = req.params.username;
+  return fetchUserByUsername(username)
+    .then((user) => {
+      res.send({ user });
     })
     .catch((err) => {
       next(err);
