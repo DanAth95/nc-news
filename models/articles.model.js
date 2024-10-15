82a380243592c16241a7d25eeca04d651100db32
DanAth95 <daniel_atherton@aol.co.uk>
2024-10-15 10:53:57 +0100

    add topic query to get articles

--- models/articles.model.js
+++ models/articles.model.js
@@ -23,6 +23,14 @@ exports.fetchArticles = (query) => {
   ];
   const allowedOrder = ["asc", "desc"];
 
+  let sql = `SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.article_id) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id`;
+  const queryVals = [];
+
+  if (query.topic) {
+    sql += ` WHERE articles.topic = $1`;
+    queryVals.push(query.topic);
+  }
+
   if (!query.sort_by) {
     query.sort_by = "created_at";
   }
@@ -37,15 +45,16 @@ exports.fetchArticles = (query) => {
     return Promise.reject({ status: 400, msg: "Invalid Query" });
   }
 
-  return db
-    .query(
-      `SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.article_id) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY articles.${
-        query.sort_by
-      } ${query.order.toUpperCase()}`
-    )
-    .then(({ rows }) => {
-      return rows;
-    });
+  sql += ` GROUP BY articles.article_id ORDER BY articles.${
+    query.sort_by
+  } ${query.order.toUpperCase()}`;
+
+  return db.query(sql, queryVals).then(({ rows }) => {
+    if (rows.length === 0) {
+      return Promise.reject({ status: 404, msg: "Topic Not Found" });
+    }
+    return rows;
+  });
 };
 
 exports.updateArticle = (update, id) => {
