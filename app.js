d6ffb5175dfec3820b0b8c2d53366560101c7a8b
DanAth95 <daniel_atherton@aol.co.uk>
2024-11-04 09:33:26 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
 const apiRouter = require("./routers/api-router");
 const topicsRouter = require("./routers/topics.router");
@@ -7,6 +7,7 @@ const usersRouter = require("./routers/users.router");
 const commentsRouter = require("./routers/comments.router");
 const articlesRouter = require("./routers/articles.router");
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", apiRouter);
 app.use("/api/topics", topicsRouter);
